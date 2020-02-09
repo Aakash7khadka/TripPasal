e9ea55212e582e4104ec0744044277763fa36790
Aakash7khadka <aakash7khadka@gmail.com>
2020-02-09 22:09:48 +0545

    some card and database modifications(model methods)

--- flight/models.py
+++ flight/models.py
@@ -7,3 +7,26 @@ class airlines(models.Model):
     destination=models.CharField(max_length=30)
     departure_time=models.IntegerField()
     price=models.IntegerField()
+
+    def __str__(self):
+        return self.airlines_name+" "+self.source+"-"+self.destination
+
+    def cutprice(self):
+        return self.price+(self.price*0.1)
+
+    def depart_time(self):
+        
+        f=int(self.departure_time/100)
+        l=int(self.departure_time%100)
+        f=str(f)
+        if(l==0):
+            l="00"
+        else:
+            l=str(l)
+        if (self.departure_time <1200):
+            return f+":"+l+" "+"a.m"
+        else:
+            return f+":"+l+" "+"p.m"
+
+def nametest(self):
+    return self.airlines_name
