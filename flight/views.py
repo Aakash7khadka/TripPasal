ff3c017d327535ff2b456add1951fd2f73d154a6
Aakash7khadka <aakash7khadka@gmail.com>
2020-02-09 19:27:03 +0545

    domestic flight complete

--- flight/views.py
+++ flight/views.py
@@ -7,7 +7,9 @@ def showflight(request):
     second_dest=request.POST['second_dest']
     first_time=int(request.POST['first_time'])
     second_time=int(request.POST['second_time'])
-    plane=airlines.objects.filter(source=first_dest).filter(destination=second_dest).order_by('price')
+    ft=first_time
+    st=second_time
+    plane=airlines.objects.filter(source=first_dest).filter(destination=second_dest).order_by('price').filter(departure_time__gte=first_time).filter(departure_time__lte=second_time)
 
     return render(request,"flightcomp.html",{'plane':plane})
 
